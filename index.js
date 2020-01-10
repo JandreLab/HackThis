6f0533b6f2a210b60dfc318ee54277f52b2131aa
JandreLab <jandre.labuschagne@epiuse.com>
2020-01-10 11:24:50 +0200

    Image Crop

---
+++ index.js
@@ -0,0 +1,41 @@
+const fse = require('fs-extra')
+const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+let results = []
+const sharp = require('sharp');
+
+function OpenALPR()
+{
+    let originalImage = 'images/image (1).png';
+
+// file name for cropped image
+let outputImage = 'res/image (1).png';
+
+sharp(originalImage).extract({ width: 400, height: 36, left: 1100, top: 1070 }).toFile(outputImage)
+    .then(function(new_file_info) {
+        console.log("Image cropped and saved");
+    })
+    .catch(function(err) {
+        console.log(err);
+    });
+var secret_key = "sk_73dc60d13cc6aa1ff8b9a613";
+var url = "https://api.openalpr.com/v2/recognize_bytes?recognize_vehicle=1&country=za&secret_key=" + secret_key;
+var xhr = new XMLHttpRequest();
+xhr.open("POST", url);
+
+var file = new Buffer(fse.readFileSync("images/image (1).png")).toString('base64'); 
+// console.log(file)
+
+// Send POST data and display response
+xhr.send(file);  // Replace with base64 string of an actual image
+xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4) {
+        results.push(JSON.parse(xhr.responseText).results[0].plate)
+        console.log(results)
+        // document.getElementById("response").innerHTML = xhr.responseText;
+    } else {
+        console.log("Waiting on response...")
+        // document.getElementById("response").innerHTML = "Waiting on response...";
+    }
+}
+}
+OpenALPR()
